69ce778a1360ab13f28d144c00c755e3f385ccd6
demitryedwards <dxyz773@gmail.com>
2023-03-15 18:13:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+const myName = "Demitry";
+
+function addName(name) {
+  newHeader.textContent = `${name} is the champion`;
+}
+
+addName(myName);
